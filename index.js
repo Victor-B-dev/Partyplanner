257c21feb6d0a578adaa43039cb51e606f9c1f68
Victor <ban.victor728@gmail.com>
2024-07-12 15:18:02 -0400

    added try catch. cleaned up psuedocode so easier to come back to read.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-
-
-// push the whole API call in a try catch.
-
 // create a state to hold information from api call
 const state = {
   allEvents: [],
@@ -15,18 +11,24 @@ const main = document.querySelector('main')
 // it will store this data into state
 // extra note - we do want to do the call renderEvents() here otherwise the call stack doesn't work in the correct order. had error for a moment
 // we separate out the render events function into another block of code for easier readability.
+// push the whole API call in a try catch.
 const getEvents = async () => {
-  const response = await fetch(`https://fsa-crud-2aa9294fe819.herokuapp.com/api/2406-FTB-ET-WEB-FT/events`);
-  const events = await response.json();
+  try {
+    const response = await fetch(`https://fsa-crud-2aa9294fe819.herokuapp.com/api/2406-FTB-ET-WEB-FT/events`);
+    const events = await response.json();
 
-  state.allEvents = events.data;
-  renderEvents();
+    state.allEvents = events.data;
+    renderEvents();
+  } catch (err){
+    console.log(err);
+  }
 }
 
 // loop through each event stored in state array and display the information.
 // create an unordered list for the events.
 // displayed information will need to be in literals since we are adding each event as list item
 // add the UL into the HTML <main>.
+// ul here is fine for now since this is only time we are creating element for now.
 const renderEvents = () => {
   const ul = document.createElement('ul');
   const eventInfo = state.allEvents.map((singleEvent) => {
@@ -41,11 +43,5 @@ const renderEvents = () => {
   main.append(ul);
 }
 
+// run the code
 getEvents();
-
-
-
-
-
-
-
