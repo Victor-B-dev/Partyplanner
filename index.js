d523ec0c596280d1c8b826deb21c4f538d0a4e7f
Victor <ban.victor728@gmail.com>
2024-07-12 19:17:39 -0400

    Refactor HTML & JS  to dynamically support adding events. However event additions are only local. rewrote some psuedo code so i can come back to it later & to remember issues i had. wrote psuedo code for delete button & possible further refactor needed

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 // create a state to hold information from api call
 const state = {
   allEvents: [],
-  selectedEvent: {}
+  newEvent: {}
 }
 
 // dom selectors
 const main = document.querySelector('main')
+const ul = document.querySelector('ul')
+const addEventButton = document.querySelector(`button`)
+
 
 // create function to do API call.
 // it will store this data into state
 // extra note - we do want to do the call renderEvents() here otherwise the call stack doesn't work in the correct order. had error for a moment
 // we separate out the render events function into another block of code for easier readability.
-// push the whole API call in a try catch.
+// put the whole API call in a try catch.
 const getEvents = async () => {
   try {
     const response = await fetch(`https://fsa-crud-2aa9294fe819.herokuapp.com/api/2406-FTB-ET-WEB-FT/events`);
@@ -24,13 +27,9 @@ const getEvents = async () => {
   }
 }
 
-// loop through each event stored in state array and display the information.
-// create an unordered list for the events.
-// displayed information will need to be in literals since we are adding each event as list item
-// add the UL into the HTML <main>.
-// ul here is fine for now since this is only time we are creating element for now.
+// loop through each event stored in state array
+// return displayed information will need to be in literals since we are adding each event as list item
 const renderEvents = () => {
-  const ul = document.createElement('ul');
   const eventInfo = state.allEvents.map((singleEvent) => {
     return `<li> Name:${singleEvent.name} <br>
             Description: ${singleEvent.description} <br>
@@ -38,10 +37,39 @@ const renderEvents = () => {
             Location: ${singleEvent.location} </li>`;
   });
 
-  
   ul.innerHTML = (eventInfo.join(``));
-  main.append(ul);
 }
+
+// when form submit is clicked, take form information and put it into state.newEvent object
+// add the object to state.allEvents array
+// run renderEvents to update page
+
+// note this add event is only locally updating the page, not adding it to the database. nor does it check if the event already exists.
+addEventButton.addEventListener('click', (event)=> {
+  event.preventDefault();
+
+  let inputName = document.querySelector('#addEventName').value;
+  let inputDescript = document.querySelector('form #addEventDescript').value;
+  let inputDate = document.querySelector('form #addDate').value;
+  let inputLoc = document.querySelector('form #addEventLoc').value;
+
+  newEvent = {
+    name: inputName,
+    description: inputDescript,
+    date: inputDate,
+    location: inputLoc,
+  }
+
+  state.allEvents.push(newEvent);
+  renderEvents();
+})
 
 // run the code
 getEvents();
+
+
+// regarding extra rubric critera. adding a delete button we would want to include the button when rendering each list items
+//so this is line 34-38 we could include something like <br> <button>Delete This Party</button>
+// write a separate function/DOM to select the clicked on LI and then remove that LI item from the state array.
+// would need to add additional wrapper that isn't rendered so we could include the index # for example and use that to select the proper item from the array
+// then render update i guess.
